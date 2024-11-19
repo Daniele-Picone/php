02371f5d7ac0b0d6ff06faf1ab78e9bbb5567c37
Daniele Picone <danielepicone97@gmail.com>
2024-11-19 15:23:46 +0100

    selfwork6

---
+++ selfwork6.php
@@ -0,0 +1,23 @@
+<?php
+
+
+for($i=1 ; $i <= 100 ; $i++){
+if ($i % 3 == 0 && $i % 5 == 0){
+ echo  " hackademi \n " ;
+}else if ($i % 3 == 0){
+    echo "php \n "  ; 
+
+   }else if ( $i % 5 == 0){
+
+    echo "php \n " ;
+}else{
+
+    echo $i ." \n";
+
+}
+}
+
+
+
+
+
