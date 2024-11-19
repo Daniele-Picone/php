b77d72327e76f8a58f6f8dc2353bc1fa50f19a51
Daniele Picone <danielepicone97@gmail.com>
2024-11-19 11:46:14 +0100

    selfowork1

---
+++ php_01_Daniele_Picone.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$int = 25;
+$float = 45.36 ;
+$string= 'Ciao Daniele';
+$bool = true;
+
+
+var_dump( $int, $float, $string , $bool );
+
+const INTEGER = $string;
+const FLOAT = $float;
+const STRING = $string;
+const BOOL = $bool;
