04933207afb1986931b3e0eaed1a5297844ec09b
Daniele Picone <danielepicone97@gmail.com>
2024-11-19 12:10:01 +0100

    selfwork2

--- php_02_Daniele_Picone.php
+++ php_02_Daniele_Picone.php
@@ -6,9 +6,11 @@ $text2 = "Hai";
 $text3 = "sete";
 $text4 = "?";
 $text5 = "Perchè";
-$text6 = $text2;
+$text6 = "$text2";
 $text7 = 'bevuto';
 $text8 = "tutto";
 
 
-echo $text1, $text2, $text3, $text4 ,$text5, $text6, $text7, $text8; 
+$frasecompleta = $text1 ." ".$text2 ." ". $text3 . " ". $text4 . " ". $text5. " ". $text6." ". $text7. " ". $text8 ;
+
+echo $frasecompleta;
