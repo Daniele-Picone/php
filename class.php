eb86bd31029b7fdc7f56466d10867e48f254e956
Daniele Picone <danielepicone97@gmail.com>
2024-11-28 17:45:00 +0100

    SelfworkOOp5

---
+++ class.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+
+
+abstract class Category{
+
+ abstract public function getMyCategory();
+
+}
+
+
+ 
+
+
+
+
+class Attualità extends Category{
+     public function getMyCategory(){
+      echo "categoria: Attualità \n";
+    }
+}
+
+class Sport extends Category{
+    public function getMyCategory(){
+      echo "Categoria: Sport \n";
+    }
+}
+
+class Gossip extends Category{
+     public function getMyCategory(){
+       echo "Categoria gossip \n";
+    }
+}
+
+class Storia extends Category{
+   public function getMyCategory(){
+      echo "Categoria : Storia \n";
+    }
+}
+
+$attulità = new Attualità();
+$sport = new Sport ();
+$gossip = new Gossip();
+$Storia = new Storia();
+
+
+$attulità->getMyCategory();
+$sport->getMyCategory();
+$gossip->getMyCategory();
+$Storia->getMyCategory();
